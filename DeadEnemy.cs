ff4cb012749656e4db0f534523a6ca6777837188
you-insert-username-here <gbrown31@myune.edu.au>
2018-08-11 21:58:45 +0930

    Add files via upload

--- DeadEnemy.cs
+++ DeadEnemy.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class DeadEnemy : MonoBehaviour 
 {
 }
---
+++ GameLoop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    public Transform[] boardTile;
+    public Transform[] edgeTile;
+
+    public int rows, columns;
+
+    private GameObject gameBoard;
+
+    private void Start()
+    {
+        //edgeTile = new Transform[4];
+        gameBoard = new GameObject("Game Board");
+
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                if (j == 0)
+                    Instantiate(edgeTile[0], new Vector2(j, i), Quaternion.identity, gameBoard.transform);
+                else if (j == rows - 1)
+                    Instantiate(edgeTile[1], new Vector2(j, i), Quaternion.identity, gameBoard.transform);
+                else if (i == 0)
+                    Instantiate(edgeTile[2], new Vector2(j, i), Quaternion.identity, gameBoard.transform);
+                else if (i == columns - 1)
+                    Instantiate(edgeTile[3], new Vector2(j, i), Quaternion.identity, gameBoard.transform);
+                else
+                    Instantiate(boardTile[Random.Range(0,3)], new Vector2(j, i), Quaternion.identity, gameBoard.transform);
+            }
+        }
+
+
+    }
+
+}
